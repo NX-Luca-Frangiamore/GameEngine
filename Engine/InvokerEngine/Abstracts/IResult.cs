3ac7dc4a56ef2fdb6ada3ffd05548dbf5bbd9952
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2024-03-08 17:09:30 +0100

    refactor: Iresult pulizia

--- Engine/InvokerEngine/Abstracts/IResult.cs
+++ Engine/InvokerEngine/Abstracts/IResult.cs
@@ -1,20 +1,5 @@
 namespace GameEngine.Engine.InvokerEngine.Abstracts;
 
-public abstract class IResult {
-    public Dictionary<string,object>Results=[];
-    public void AddResults<T>(string key,T data)=>Results[key]=data!;
-    public T? Get<T>(string key){
-        if(!Results.TryGetValue(key, out object? value)) return default;
-        try{
-            return (T)value;
-        }
-        catch (Exception)
-        {
-            return default;
-        }
-    }
-    public override string ToString(){
-        return Results.ToString()!;
-}}
+public interface IResult {}
 public class DefaultResult : IResult { 
 }
